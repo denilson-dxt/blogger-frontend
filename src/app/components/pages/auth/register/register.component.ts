787c0d87f9c931bb096fd64b64103fd94d178d5f
Denilson Tivane <denilsontivane@gmail.com>
2022-12-09 11:36:36 +0200

    Add register

--- src/app/components/pages/auth/register/register.component.ts
+++ src/app/components/pages/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm!:FormGroup;
+  constructor(private authService:AuthService, private router:Router) { }
 
   ngOnInit(): void {
+    this._initializeForm();
+  }
+
+  private _initializeForm(){
+    this.registerForm = new FormGroup({
+      email:new FormControl("", [Validators.required, Validators.email]),
+      userName: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)]),
+      file:new FormControl(""),
+      profilePicture: new FormControl("")
+
+    })
+  }
+
+  onFileChange(event:any){
+      if(event.target.files.length > 0){
+        const file = event.target.files[0];
+        this.registerForm.patchValue({
+          profilePicture: file
+        })
+      }
+  }
+  submit(){
+    if(this.registerForm.invalid) return;
+    const formData = new FormData();
+    formData.append("email", this.registerForm.get("email")?.value);
+    formData.append("userName", this.registerForm.get("userName")?.value);
+    formData.append("password", this.registerForm.get("password")?.value);
+    formData.append("profilePicture", this.registerForm.get("profilePicture")?.value);
+    console.log(this.registerForm.value);
+    
+    this.authService.register(formData).subscribe(data =>{
+      console.log(data);
+      this.router.navigate(["/auth/login"])
+    });
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,8 @@ import {HttpClient} from "@angular/common/http"
 import { ILoginResponse } from '../interfaces/login-response';
 import { ILoginData } from '../interfaces/login-data';
 import { environment } from 'src/environments/environment';
+import { IRegisterData } from '../interfaces/register-data';
+import { IRegisterResponse } from '../interfaces/register-response';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,11 @@ export class AuthService {
 
   login(data:ILoginData):Observable<ILoginResponse>{
     return this.http.post<ILoginResponse>(`${environment.api}/auth/login`, data);
+  }
+
+  register(data:FormData):Observable<IRegisterResponse>{
+    console.log("Service", data);
+    
+    return this.http.post<IRegisterResponse>(`${environment.api}/auth/register`, data);
   }
 }
