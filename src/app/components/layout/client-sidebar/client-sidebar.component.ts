9618b69016a390917a08192c74db91cc4964c9f4
Denilson Tivane <denilsontivane@gmail.com>
2022-12-21 11:17:30 +0200

    list tags on client sidebar implemented

--- src/app/components/layout/client-sidebar/client-sidebar.component.ts
+++ src/app/components/layout/client-sidebar/client-sidebar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { ICategory } from 'src/app/interfaces/category';
+import { ITag } from 'src/app/interfaces/tag';
+import { IAppState } from 'src/app/store/reducers';
+import { selectAllCategories } from 'src/app/store/selectors/category.selector';
+import { selectAllTags } from 'src/app/store/selectors/tag.selectors';
 
 @Component({
   selector: 'app-client-sidebar',
@@ -6,13 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-sidebar.component.css']
 })
 export class ClientSidebarComponent implements OnInit {
-getImageAsBackgroundUrl(src: string): any {
-  return `url('${src}')`;
-}
-
-  constructor() { }
-
+  
+  categories:ICategory[] = [];
+  tags:ITag[] = [];
+  constructor(private store:Store<IAppState>) { }
+  
   ngOnInit(): void {
-  }
+    this.store.pipe(select(selectAllCategories)).subscribe(categories => {
+      this.categories = categories;
+    });
 
+    this.store.pipe(select(selectAllTags)).subscribe(tags => {
+      this.tags = tags;
+    })
+
+  }
+  
+  getImageAsBackgroundUrl(src: string): any {
+    return `url('${src}')`;
+  }
 }
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { IPost } from 'src/app/interfaces/post';
+import { getAllCategories } from 'src/app/store/actions/category.actions';
 import { getAllPosts } from 'src/app/store/actions/post.actions';
+import { getAllTags } from 'src/app/store/actions/tag.actions';
 import { selectAllPosts } from 'src/app/store/selectors/post.selectors';
 
 @Component({
@@ -15,6 +17,8 @@ posts:IPost[] = [];
   constructor(private store:Store) { }
 
   ngOnInit(): void {
+    this.store.dispatch(getAllCategories())
+    this.store.dispatch(getAllTags())
     this.store.dispatch(getAllPosts());
     this.store.pipe(select(selectAllPosts)).subscribe(posts => {
       this.posts = [...posts];
